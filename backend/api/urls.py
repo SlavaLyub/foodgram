c5e99ef462e5ae79d339d3360417f1bbed042db1
slavalyub <v.lyub4enko@mail.ru>
2024-08-18 03:42:45 +0300

    расширил djoser, осталось дописать вычисляемое поле с подписками

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import UserListView, UserAvatarUpdateView
+from .views import UserAvatarUpdateView
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
-    path('users/', UserListView.as_view(), name='user_list'),
+    #  path('users/', UserListView.as_view(), name='user_list'),
     path('', include('djoser.urls')),  # TODO: Если этот эндпоинт выше 5-й строчки, он перехватыват вывод пользователей
     path('users/me/avatar/', UserAvatarUpdateView.as_view(), name='user_avatar_update'),
 ]
